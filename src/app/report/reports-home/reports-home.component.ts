e3a1fa3ed53a73e91103e8351bf20b1f781cbdaf
Spam7 <shubhammunjal74@gmail.com>
2019-05-06 16:25:44 +0530

    date

--- src/app/report/reports-home/reports-home.component.ts
+++ src/app/report/reports-home/reports-home.component.ts
@@ -1,25 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+import {NgbDateStruct, NgbCalendar, NgbDate} from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
-import { CustomerService } from 'src/app/customer/customer.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-reports-home',
   templateUrl: './reports-home.component.html',
-  styleUrls: ['./reports-home.component.css']
+  styleUrls: ['./reports-home.component.css'],
+  providers: [DatePipe]
 })
 export class ReportsHomeComponent implements OnInit {
   
   model: NgbDateStruct;
-  date: {year: number, month: number,day: number};
+  date: {year: number, month: number};
+  i;
+  myDate = new Date();
+  currdate = this.datePipe.transform(this.myDate, 'dd-mm');
+  constructor(private calendar: NgbCalendar,public router:Router,private datePipe: DatePipe) { 
+   
+  }
 
-  constructor(private calendar: NgbCalendar,public router:Router,public cS:CustomerService) { }
+  isDisabled = (date: NgbDate, current: {month: number}) => (date.day) > parseInt( this.currdate);
 
   ngOnInit() {
+    // for(this.i=this.currdate;this.i>=this.currdate;this.i++){
+      console.log(this.currdate);
+    //   // const isDisabled = (date: NgbDate, current: {month: number}) => date.day === 13;
+    // }
   }
+
+
   generateReport(sDate){
+    console.clear();
     console.log("Log cleared at generate Report")
     console.log(sDate);
-    this.router.navigate(['/home/reports/wingWiseReport'],{queryParams: sDate })
+    this.router.navigate(['/reports/wingWiseReport'],{queryParams: sDate })
   }
 }
+ 
